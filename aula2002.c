d5fb9d1006394e452515c0eb05152a60fa871a86
Felipe Lopes <felipelipelop@gmail.com>
2024-02-26 21:08:14 -0300

    arquivos da disciplina Programação II do 2º periodo do curso de ads

---
+++ aula2002.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void main(){
+	int a = 30;
+	printf("o valor e %d", a);
+}
---
+++ aula2602.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main(){
+	int n = 2;
+	float m = 3.6;
+	printf("valor de  n: %d e valor de m: %f", n, m);
+	printf("\nMostrou os valores");
+}
